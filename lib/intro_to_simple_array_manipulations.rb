0c59fb07f2363f3e9d37d1b3041f1442de052fae
Cindy Vo <yendai.vo@hotmail.com>
2018-07-11 04:25:58 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,45 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop
+  array.pop
+end
+
+def shift_with_args(array)
+  array.shift
+  array.shift
+end
+
+def using_concat(array, array2)
+  array3 = array.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, num)
+  array.delete_at(num)
+end
